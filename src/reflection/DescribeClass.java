27f62fe9e7289ba7b422b8c12144dc9e15bb5acd
FabianZ1990 <fabian.zischler@gmail.com>
2023-02-09 20:53:46 +0000

    redid describe class

--- src/reflection/DescribeClass.java
+++ src/reflection/DescribeClass.java
@@ -2,15 +2,37 @@ package reflection;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+
 
 public class DescribeClass {
-    public static void main(String[] args) throws NoSuchMethodException {
-        if (args.length != 1)
-            System.out.println("Usage: "); // TODO: describe how to use the utility
-        Scanner in = new Scanner(System.in);
-        String s = in.nextLine();
-        System.out.println(String.class.getName());
+    public static void main(String[] args) {
+
+        if (args.length > 0){
+
+            try {
+
+                for (String classes : args) {
+                    System.out.println(classes);
+                Class<?> testClass = Class.forName(classes);
+                getConstructorInfo(testClass);
+                getMethodInfo(testClass);
+                getModifierInfo(testClass); }
+
+
+
+            } catch (ClassNotFoundException e) {
+                System.out.println("Class not found");
+            }
+        }
+
+
+
+/*
+        String s = args[0];
+        //System.out.println(String.class.getName());
         Class<?> x;
         try {
             x = (java.lang.Class.forName(s));
@@ -29,7 +51,22 @@ public class DescribeClass {
         for (Method y : methods) {
             System.out.println(y);
         }
+*/
 
         // TODO: implement the functionality for Question 1
+    }
+
+    private static void getModifierInfo(Class<?> testClass) {
+        System.out.println(testClass.getModifiers());
+    }
+
+    private static void getMethodInfo(Class<?> testClass) {
+        System.out.println(testClass.getMethods().length);
+        System.out.println(Arrays.stream(testClass.getMethods()).limit(4).collect(Collectors.toList()));
+    }
+
+    private static void getConstructorInfo(Class<?> testClass) {
+        System.out.println(testClass.getConstructors().length);
+        System.out.println(Arrays.stream(testClass.getConstructors()).limit(4).collect(Collectors.toList()));
     }
 }
